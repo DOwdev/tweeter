c339024dc3fbbe719c4b4a71456a36954facdf0f
DOwdev <63557295+DOwdev@users.noreply.github.com>
2021-04-04 21:57:04 -0400

    implement char-counter

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#tweet-text").on("input", function(){
+        let $counter = $(this).parent().find(".counter");
+        let count = 140 - this.value.length;
+        $counter.html(count);
+        if(count > 0){
+            $counter.removeClass("red");
+        } else{
+            $counter.addClass("red");
+        }
+    })
+  });
