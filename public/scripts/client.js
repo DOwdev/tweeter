0977c975a1ae7887916253fc51a3be1149debf99
DOwdev <63557295+DOwdev@users.noreply.github.com>
2021-04-07 21:34:17 -0400

    update

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,33 +8,6 @@
 
 $(document).ready(() => {
 
-    // Test / driver code (temporary). Eventually will get this from the server.
-const data = [
-        {
-          "user": {
-            "name": "Newton",
-            "avatars": "https://i.imgur.com/73hZDYK.png"
-            ,
-            "handle": "@SirIsaac"
-          },
-          "content": {
-            "text": "If I have seen further it is by standing on the shoulders of giants"
-          },
-          "created_at": 1461116232227
-        },
-        {
-          "user": {
-            "name": "Descartes",
-            "avatars": "https://i.imgur.com/nlhLi3I.png",
-            "handle": "@rd" },
-          "content": {
-            "text": "Je pense , donc je suis"
-          },
-          "created_at": 1461113959088
-        }
-      ]
-    
-
  const createTweetElement = function(tweetObject){
     const $tweet = $(`<article>
     <div class="header">
@@ -44,7 +17,7 @@ const data = [
       </div>
       <span class="username-tweet">${tweetObject.user.handle}</span>
     </div>
-    <p>${tweetObject.content.text}</p>
+    <p>${escape(tweetObject.content.text)}</p>
     <footer>
       <span>${new Date(tweetObject.created_at)}</span>
       <div class="icons">
@@ -58,22 +31,40 @@ const data = [
   return $tweet;
 }
 
+const escape =  function(str) {
+    let div = document.createElement('div');
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  }
+
 const renderTweets = function(tweets){
     for(let tweet of tweets){
         let $tweet = createTweetElement(tweet);
         $('#tweets-container').prepend($tweet);
     }
 }
-//not finished
-// $(".form-button").on('click', function () {
-//     console.log('Button clicked, performing ajax call...');
-//     $.ajax('more-posts.html', { method: 'GET' })
-//     .then(function (morePostsHtml) {
-//       console.log('Success: ', morePostsHtml);
-//       $button.replaceWith(morePostsHtml);
-//     });
-//   });
 
-renderTweets(data);
+function loadTweets(){
+    $.ajax('/tweets', {method: 'GET', dataType: "json"})
+    .then(result =>{
+        renderTweets(result);
+    })
+}
+
+loadTweets();
+
+$("#tweet-form").submit( function (event) {
+    event.preventDefault();
+    let text = $("#tweet-text").val();
+    if(text === "" || text === null){
+        alert("Message is empty");
+    } else if(text.length > 140){
+        alert("Maximum of 140 chars");
+    }else{
+        $.ajax('/tweets', { method: 'POST', dataType: "json", data: {"text": text} })
+        location.reload();
+    }
+  });
+
 
 })
