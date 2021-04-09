8024adef6ab2db206b325d1d47e9fb68bbaf2bcc
DOwdev <63557295+DOwdev@users.noreply.github.com>
2021-04-08 22:04:37 -0400

    update for media queries and other CSS changes

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -8,7 +8,7 @@
 
 $(document).ready(() => {
 
- const createTweetElement = function(tweetObject){
+  const createTweetElement = function(tweetObject) {
     const $tweet = $(`<article>
     <div class="header">
       <div class="user-tweet">
@@ -28,43 +28,88 @@ $(document).ready(() => {
     </footer>
   </article>`);
 
-  return $tweet;
-}
+    return $tweet;
+  };
 
-const escape =  function(str) {
+  const escape =  function(str) {
     let div = document.createElement('div');
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
+  };
+
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      let $tweet = createTweetElement(tweet);
+      $('#tweets-container').prepend($tweet);
+    }
+  };
+
+  function loadTweets() {
+    $.ajax('/tweets', {method: 'GET', dataType: "json"})
+      .then(result =>{
+        renderTweets(result);
+      });
   }
 
-const renderTweets = function(tweets){
-    for(let tweet of tweets){
-        let $tweet = createTweetElement(tweet);
-        $('#tweets-container').prepend($tweet);
-    }
-}
+  loadTweets();
 
-function loadTweets(){
-    $.ajax('/tweets', {method: 'GET', dataType: "json"})
-    .then(result =>{
-        renderTweets(result);
-    })
-}
-
-loadTweets();
-
-$("#tweet-form").submit( function (event) {
+  $("#tweet-form").submit(function(event) {
     event.preventDefault();
     let text = $("#tweet-text").val();
-    if(text === "" || text === null){
-        alert("Message is empty");
-    } else if(text.length > 140){
-        alert("Maximum of 140 chars");
-    }else{
-        $.ajax('/tweets', { method: 'POST', dataType: "json", data: {"text": text} })
-        location.reload();
+    if (text === "" || text === null) {
+      let error = "Tweet Cannot be Empty";
+      $(".error-container").empty().prepend(errorMessage(error)).hide();
+      if ($(".error-container").is(":hidden")) {
+        $(".error-container").slideDown("slow");
+      }
+    } else if (text.length > 140) {
+      let error = "Tweet Exceeded Max Number of Characters";
+      $(".error-container").empty().prepend(errorMessage(error)).hide();
+      if ($(".error-container").is(":hidden")) {
+        $(".error-container").slideDown("slow");
+      }
+    } else {
+      $(".error-container").hide();
+      $.ajax('/tweets', { method: 'POST', dataType: "json", data: {"text": text} });
+      location.reload();
     }
   });
 
+  $('.btn-compose-tweet').click(function() {
+    let $section = $('section.new-tweet');
 
-})
+    if ($section.is(':visible')) {
+      $section.slideUp('fast');
+    } else {
+      $section.slideDown('fast');
+      $section.find('textarea').focus();
+    }
+  });
+
+  $(window).scroll(function() {
+    $('.nav-right').hide();
+    $('.fixed-button').show();
+    if ($(window).scrollTop() < 120) {
+      $('.nav-right').show();
+      $('.fixed-button').hide();
+    }
+  });
+
+  $('.fixed-button').click(function() {
+    $(window).scrollTop(0);
+  });
+
+});
+
+const errorMessage = function(error) {
+  const $error = $(`
+    <div class="error">
+      <div class="error-logo">
+      <i class="fas fa-exclamation-triangle"></i>
+      <h3>Error:</h3>
+    </div>
+    <p>${error}</p>
+    </div>
+  `);
+  return $error;
+};
